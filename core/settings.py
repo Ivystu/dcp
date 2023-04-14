fa39bab25e1762f5603b89522bc0232b9d1b0bd3
Ivystu <ivystu0315@gmail.com>
2023-04-14 18:11:12 +0800

    加逗號

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dcp.apps.DcpConfig'
+    'dcp.apps.DcpConfig',
     'dcp'
 ]
 
